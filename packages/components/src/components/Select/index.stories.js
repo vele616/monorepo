887dd5002f36e0097b1f39e2aea0bd5b7f14ac09
IvanPenga <ivan.penga2@gmail.com>
2020-11-23 14:36:41 +0100

    clean select story template

--- packages/components/src/components/Select/index.stories.js
+++ packages/components/src/components/Select/index.stories.js
@@ -1,21 +1,18 @@
-import React from 'react';
-import Select from './index';
+import React from "react";
+import Select from "./index";
 
 export default {
-  title: 'Components/Select',
+  title: "Components/Select",
   component: Select,
 };
 
-const Template = (args) => <>
-  <Select {...args}>
-    <Select.Item>Option 1</Select.Item>
-    <Select.Item>Option 2</Select.Item>
-    <Select.Item>Option 3</Select.Item>
-    <Select.Item>Option 4</Select.Item>
-  </Select>
-</>;
+const Template = (args) => (
+  <>
+    <Select {...args}></Select>
+  </>
+);
 
 export const Primary = Template.bind({});
 Primary.args = {
-  label: 'Country'
+  label: "Country",
 };
