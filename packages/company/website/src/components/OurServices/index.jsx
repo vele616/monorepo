7f14a332457320982ba7bb2ac8798774163188be
DanicaPivalica <danica.pivalica@gmail.com>
2020-11-24 17:02:08 +0100

    extracting single components for offers

--- packages/company/website/src/components/OurServices/index.jsx
+++ packages/company/website/src/components/OurServices/index.jsx
@@ -1,17 +1,14 @@
 import React, { useState } from "react";
 import { StaticQuery, graphql } from "gatsby";
-import {
-  Typography,
-  Section,
-  Card,
-  Grid,
-  Button,
-  Flexbox,
-} from "@crocoder-dev/components";
+import { Typography, Section, Grid } from "@crocoder-dev/components";
 import styles from "./index.module.scss";
+import { OffersTitle, OfferCard, OfferHeadline } from "./Offer";
 
 const OurServices = ({ title, startups, business }) => {
   const [visibleBusiness, setVisibleBusiness] = useState(false);
+
+  const showStartup = React.useCallback(() => setVisibleBusiness(false), []);
+  const showBusiness = React.useCallback(() => setVisibleBusiness(true), []);
 
   return (
     <Section
@@ -34,144 +31,47 @@ const OurServices = ({ title, startups, business }) => {
         className={styles.grid}
         justifyContent="space-between"
       >
-        <Button
-          onClick={() => setVisibleBusiness(false)}
-          variant="sneaky"
+        <OffersTitle
+          onClick={showStartup}
           className={styles.startupTitle}
+          isActive={!visibleBusiness}
         >
-          <Typography
-            color="gray_2"
-            fontFamily="rubik"
-            fontSize={30}
-            fontWeight={300}
-            className={`${!visibleBusiness && styles.active}`}
-          >
-            {startups.title}
-          </Typography>
-        </Button>
+          {startups.title}
+        </OffersTitle>
+
         {startups.offer.map((offer) => (
-          <Card
-            narrow
+          <OfferCard
+            {...offer}
             key={offer.title}
-            className={`${styles.card} ${
-              startups.offer.length > 1 && styles.small
-            } ${!visibleBusiness && styles.visible}`}
-          >
-            <Typography
-              element="div"
-              fontWeight={600}
-              style={{ marginBottom: "15px" }}
-              fontSize={34}
-              color="gray_2"
-              fontFamily="rubik"
-            >
-              {offer.title}
-            </Typography>
-            <Typography
-              fontSize={18}
-              style={{ lineHeight: "1.6" }}
-              color="gray_11"
-            >
-              <div dangerouslySetInnerHTML={{ __html: offer.text }} />
-            </Typography>
-          </Card>
+            isVisible={!visibleBusiness}
+            isSmall={startups.offer.length > 1}
+          />
         ))}
-        <Flexbox
-          justifyContent="space-between"
-          alignItems="center"
-          className={`${styles.headline} ${!visibleBusiness && styles.visible}`}
-        >
-          <Typography
-            style={{ lineHeight: "1.6" }}
-            fontFamily="rubik"
-            fontSize={36}
-            fontWeight={300}
-          >
-            <Typography fontWeight={600}>
-              <em>Cro</em>Coder{" "}
-            </Typography>
-            <span dangerouslySetInnerHTML={{ __html: startups.headline }} />
-          </Typography>
-          <Button
-            style={{
-              marginLeft: "10px",
-              width: "200px",
-              flexGrow: 0,
-              flexShrink: 0,
-            }}
-          >
-            {startups.callToAction}
-          </Button>
-        </Flexbox>
-        <Button
-          onClick={() => setVisibleBusiness(true)}
-          variant="sneaky"
+        <OfferHeadline
+          callToAction={startups.callToAction}
+          headline={startups.headline}
+          isVisible={!visibleBusiness}
+        />
+        <OffersTitle
+          onClick={showBusiness}
           className={styles.businessTitle}
+          isActive={visibleBusiness}
         >
-          <Typography
-            color="gray_2"
-            fontFamily="rubik"
-            fontSize={30}
-            fontWeight={300}
-            className={`${visibleBusiness && styles.active}`}
-          >
-            {business.title}
-          </Typography>
-        </Button>
+          {business.title}
+        </OffersTitle>
         {business.offer.map((offer) => (
-          <Card
-            narrow
+          <OfferCard
+            {...offer}
             key={offer.title}
-            className={`${styles.card} ${
-              business.offer.length > 1 && styles.small
-            } ${visibleBusiness && styles.visible}`}
-          >
-            <Typography
-              element="div"
-              style={{ marginBottom: "15px" }}
-              fontSize={34}
-              color="gray_2"
-              fontFamily="rubik"
-            >
-              {offer.title}
-            </Typography>
-            <Typography
-              fontSize={18}
-              style={{ lineHeight: "1.6" }}
-              color="gray_11"
-            >
-              <div dangerouslySetInnerHTML={{ __html: offer.text }} />
-            </Typography>
-          </Card>
+            isVisible={visibleBusiness}
+            isSmall={business.offer.length > 1}
+          />
         ))}
-        <Flexbox
-          justifyContent="space-between"
-          alignItems="center"
-          className={`${styles.headline} ${visibleBusiness && styles.visible}`}
-        >
-          <Typography
-            style={{ lineHeight: "1.6" }}
-            color="gray_2"
-            fontFamily="rubik"
-            fontSize={36}
-            fontWeight={300}
-          >
-            <Typography fontWeight={600}>
-              <em>Cro</em>Coder{" "}
-            </Typography>
-            <span dangerouslySetInnerHTML={{ __html: business.headline }} />
-          </Typography>
-          <Button
-            style={{
-              marginLeft: "10px",
-              width: "200px",
-              flexGrow: 0,
-              flexShrink: 0,
-            }}
-          >
-            {business.callToAction}
-          </Button>
-        </Flexbox>
+        <OfferHeadline
+          callToAction={business.callToAction}
+          headline={business.headline}
+          isVisible={visibleBusiness}
+        />
       </Grid>
     </Section>
   );
