9ca50025ad2616dde57261fe7ed569932e34edca
DanicaPivalica <danica.pivalica@gmail.com>
2020-11-26 18:03:12 +0100

    adding close nav call to button

--- packages/company/website/src/components/Layout/index.jsx
+++ packages/company/website/src/components/Layout/index.jsx
@@ -18,12 +18,20 @@ const Layout = ({ children, stickyFooter, pageTitle, scrollToContactUs }) => {
           </Link>
         }
       >
-        <Button onClick={() => {
-          window.sa_event(`${process.env.GATSBY_SCHEDULE_CALL_NAVIGATION_CLICK_SA_EVENT}`);
-          scrollToContactUs();
-        }} variant="secondary">
-          Contact Us
-        </Button>
+        {(toggle) => (
+          <Button
+            onClick={() => {
+              toggle();
+              window.sa_event(
+                `${process.env.GATSBY_SCHEDULE_CALL_NAVIGATION_CLICK_SA_EVENT}`
+              );
+              scrollToContactUs();
+            }}
+            variant="secondary"
+          >
+            Contact Us
+          </Button>
+        )}
       </Navigation>
       {children}
       <Footer sticky={stickyFooter} />
