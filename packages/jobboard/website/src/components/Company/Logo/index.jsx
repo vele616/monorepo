14a968f679b3f685d43502de2e5bfb5920b7dcdb
David Abram <david@abram.com>
2020-11-30 19:19:29 +0100

    images/logo fix

--- packages/jobboard/website/src/components/Company/Logo/index.jsx
+++ packages/jobboard/website/src/components/Company/Logo/index.jsx
@@ -5,13 +5,21 @@ const ImageWithFilter = styled.img`
   ${({ logoUrl }) => (!!logoUrl ? '' : `filter: grayscale(100%);`)}
 `;
 
-const CompanyLogo = ({ src, className, ...other }) => (
-  <ImageWithFilter
+const CompanyLogo = ({ src, className, ...other }) => {
+
+  const [rendered, setRendered] = React.useState(false);
+
+  React.useEffect(() => {
+    setRendered(true);
+  }, []);
+
+  return (<ImageWithFilter
     {...other}
     logoUrl={src}
+    style={rendered === false ? { display: 'none' } : null }
     className={className}
     src={src || '/images/logo.png'}
-  />
-);
+  />);
+};
 
 export default CompanyLogo;
