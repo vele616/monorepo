5a87c3d27bf69dac8a3f0d133bc3e9477e156f70
IvanPenga <ivan.penga2@gmail.com>
2020-12-11 13:54:16 +0100

    add singleton memo

--- packages/components/src/hooks/useDevice.js
+++ packages/components/src/hooks/useDevice.js
@@ -1,17 +1,17 @@
-import { useState, useEffect } from 'react';
-import { desktop, tabletPortrait, mobile, viewportLimit } from '../styles/main.module.scss';
+import { useState, useEffect } from "react";
+import {
+  desktop,
+  tabletPortrait,
+  mobile,
+  viewportLimit,
+} from "../styles/main.module.scss";
 
-
+const myHooks = {};
 
 /**
  * Custom hook for screen-size detection in components.
  */
-export default function useDevice(custom) {
-  const mobileLimit = (custom && custom.mobile) || Number(mobile.replace("px",""));
-  const tabletLimit = (custom && custom.tablet) || Number(tabletPortrait.replace("px",""));
-  const desktopLimit = (custom && custom.desktop) || Number(desktop.replace("px",""));
-  const largeDesktopLimit = (custom && custom.largeDesktop) || Number(viewportLimit.replace("px",""));
-
+function useDevice(mobileLimit, tabletLimit, desktopLimit, largeDesktopLimit) {
   const [viewport, setViewport] = useState({
     isMobile: false,
     isDesktop: false,
@@ -20,8 +20,10 @@ export default function useDevice(custom) {
   });
 
   useEffect(() => {
-    const isMobile = () => window.innerWidth < tabletLimit && window.innerWidth >= mobileLimit;
-    const isTablet = () => window.innerWidth >= tabletLimit && window.innerWidth < desktopLimit;
+    const isMobile = () =>
+      window.innerWidth < tabletLimit && window.innerWidth >= mobileLimit;
+    const isTablet = () =>
+      window.innerWidth >= tabletLimit && window.innerWidth < desktopLimit;
     const isDesktop = () => window.innerWidth >= desktopLimit;
     const isLargeDesktop = () => window.innerWidth > largeDesktopLimit;
 
@@ -39,9 +41,32 @@ export default function useDevice(custom) {
         isLargeDesktop: isLargeDesktop(),
       });
     };
-    window.addEventListener('resize', resizeHanlder);
-    return () => window.removeEventListener('resize', resizeHanlder);
-  }, []);
+    window.addEventListener("resize", resizeHanlder);
+    return () => window.removeEventListener("resize", resizeHanlder);
+  }, [desktopLimit, largeDesktopLimit, mobileLimit, tabletLimit]);
 
   return viewport;
+}
+
+export default function memoizedHook(custom) {
+  const mobileLimit =
+    (custom && custom.mobile) || Number(mobile.replace("px", ""));
+  const tabletLimit =
+    (custom && custom.tablet) || Number(tabletPortrait.replace("px", ""));
+  const desktopLimit =
+    (custom && custom.desktop) || Number(desktop.replace("px", ""));
+  const largeDesktopLimit =
+    (custom && custom.largeDesktop) || Number(viewportLimit.replace("px", ""));
+
+  const deviceKey = `${mobileLimit}, ${tabletLimit}, ${desktopLimit}, ${largeDesktopLimit}`;
+  if (!myHooks[deviceKey]) {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    myHooks[deviceKey] = useDevice(
+      mobileLimit,
+      tabletLimit,
+      desktopLimit,
+      largeDesktopLimit
+    );
+  }
+  return myHooks[deviceKey];
 }
