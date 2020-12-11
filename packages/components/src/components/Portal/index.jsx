d566d17c2fd627a1b2f28e43d86c21cb09af415c
Danica Pivalica Abram <danicapivalicaabram@pop-os.localdomain>
2020-12-11 15:42:35 +0100

    wrap up Portal component initial implementation

--- packages/components/src/components/Portal/index.jsx
+++ packages/components/src/components/Portal/index.jsx
@@ -4,61 +4,16 @@ import PropTypes from "prop-types";
 import ReactDOM from "react-dom";
 import classnames from "classnames";
 import styles from "./index.module.scss";
-
-const getScreenPosition = ({ parentId, x, y, positionOffset, includeHalf }) => {
-  if (!parentId) return {};
-  const parent = document.getElementById(parentId);
-  if (!parent) return {};
-  try {
-    const boundingRect = parent.getBoundingClientRect();
-    let left;
-    let top;
-    switch (x) {
-      case "left": {
-        left = boundingRect.x;
-        break;
-      }
-      case "center": {
-        left = includeHalf
-          ? boundingRect.x + Math.round(boundingRect.width / 2)
-          : boundingRect.x;
-        break;
-      }
-      default: {
-        left = boundingRect.x + boundingRect.width;
-        break;
-      }
-    }
-    switch (y) {
-      case "top": {
-        top = boundingRect.y;
-        break;
-      }
-      case "center": {
-        top = includeHalf
-          ? boundingRect.y + Math.round(boundingRect.height / 2)
-          : boundingRect.y;
-        break;
-      }
-      default: {
-        top = boundingRect.y + boundingRect.height;
-        break;
-      }
-    }
-
-    return {
-      top,
-      left,
-      padding: `${positionOffset}px`,
-    };
-  } catch (ex) {
-    return { top: 0, left: 0, transform: undefined };
-  }
-};
+import getScreenPosition from "./getScreenPosition";
 
 /**
- * Basic wrapper to create floating components outside the
- * DOM hierarchy of the parent component.
+ * Basic wrapper to create elevated components outside the
+ * DOM hierarchy of the parent component. Useful for creating Modals, Alerts
+ * and other components that need to vertically stand out.
+ *
+ * Uses default browser behavior to determine which element is on top
+ * and places everything in the div with the id portal-layer. If such
+ * div does not exist prior, it creates it.
  */
 class Portal extends React.Component {
   constructor(props) {
@@ -69,6 +24,7 @@ class Portal extends React.Component {
       padding: `${props.positionOffset}px`,
     };
     this.setupPortalLayer = this.setupPortalLayer.bind(this);
+    this.updateScreenPosition = this.updateScreenPosition.bind(this);
   }
 
   componentDidMount() {
@@ -109,6 +65,17 @@ class Portal extends React.Component {
     this.portalLayer = portalLayer;
   }
 
+  /**
+   * Exposed to children in case they need to manually update position.
+   */
+  updateScreenPosition() {
+    const { parentId, x, y, positionOffset, includeHalf } = this.props;
+    this.setState((prev) => ({
+      ...prev,
+      ...getScreenPosition({ parentId, x, y, positionOffset, includeHalf }),
+    }));
+  }
+
   render() {
     const {
       children,
@@ -123,14 +90,16 @@ class Portal extends React.Component {
       <div
         style={this.state}
         className={classnames(className, {
-          [styles.relative]: relative,
-          [styles.base]: !relative,
+          [styles.fixed]: !relative,
+          [styles.base]: relative,
           [styles.withParent]: !!parentId,
           [styles[`${x}_${y}`]]: true,
           [styles.removeHalf]: !includeHalf,
         })}
       >
-        {children}
+        {typeof children === "function"
+          ? children(this.updateScreenPosition)
+          : children || null}
       </div>,
       this.element
     );
@@ -138,26 +107,56 @@ class Portal extends React.Component {
 }
 
 Portal.propTypes = {
+  /**
+   * Classname applied to the wrapper around all children.
+   * This wrapper has position-related CSS applied to it.
+   * If you pass a function as a child, the Portal component will
+   * pass down a function for updating screen position.
+   * This will allow you to programatically trigger recalculating of
+   * position, however you need to make sure you're not calling
+   * it way to often.
+   */
   className: PropTypes.string,
-  children: PropTypes.node.isRequired,
+  children: PropTypes.oneOfType([PropTypes.node, PropTypes.func]).isRequired,
+  /**
+   * You can use the `parentId` property to indicate what
+   * element you'd like to be relative to. In case this value
+   * is empty, you'll be positioned relatively to te screen.
+   * If this value is set, the wrapper element has absolute positioning.
+   */
   parentId: PropTypes.string,
   /**
-   * Sets the wrapper parent's position to fixed.
-   * If the parentId is supplied, the Portal will render
-   * it's children and their wrapper relative to that element.
+   * Sets the wrapper parent's position to relative (overwrites the default absolute/fixed positioning).
    */
   relative: PropTypes.bool,
+  /**
+   * Boolean property that influences the left CSS property of the
+   * wrapper element. In case it's true, the center calculations
+   * will include the parent's width and height.
+   */
   includeHalf: PropTypes.bool,
+  /**
+   * Adds an equal padding to the wrapper element. Useful when you need to
+   * create extra space between parent and children.
+   */
   positionOffset: PropTypes.number,
-  x: PropTypes.oneOf(["left", "right", "center"]),
-  y: PropTypes.oneOf(["top", "bottom", "center"]),
+  /**
+   * In case of fixed positioning, sets the position of the
+   * children on the X-axis.
+   */
+  x: PropTypes.oneOf(["left", "center", "right"]),
+  /**
+   * In case of fixed positioning, sets the position of the
+   * children on the Y-axis.
+   */
+  y: PropTypes.oneOf(["top", "center", "bottom"]),
 };
 
 Portal.defaultProps = {
   className: "",
   positionOffset: 0,
-  parentId: "my-button",
-  relative: true,
+  parentId: null,
+  relative: false,
   includeHalf: false,
   x: "center",
   y: "center",
