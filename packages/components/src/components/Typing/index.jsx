522e1b484b5ce89a894f05cd2b7c5fbf2c0710e2
IvanPenga <ivan.penga2@gmail.com>
2020-12-28 14:47:32 +0100

    add Typing component

---
+++ packages/components/src/components/Typing/index.jsx
@@ -0,0 +1,94 @@
+/* eslint-disable react/prop-types */
+import { useEffect, useRef, useState } from "react";
+import PropTypes from "prop-types";
+
+const CHARACTERS = Object.freeze({
+  BACKSPACE: "backspace",
+  WAIT: "wait",
+});
+
+const Typing = ({
+  children,
+  errorChance = 1,
+  typingInterval = 50,
+  availableLetters = "abcdefghijklmnopqrstuvwxyz",
+}) => {
+  const [text, setText] = useState("");
+  const currentCharacter = useRef(0);
+
+  useEffect(() => {
+    if (!children || typeof children !== "string") return null;
+
+    setText("");
+    currentCharacter.current = 0;
+
+    const characterSequence = children.split("");
+
+    if (errorChance >= 0 && errorChance <= 1 && children.length >= 6) {
+      if (Math.random() > 1 - errorChance) {
+        const incorrectLetterPosition =
+          Math.floor(Math.random() * (characterSequence.length - 5)) + 3;
+        const randomLetter =
+          availableLetters[Math.floor(Math.random() * availableLetters.length)];
+        characterSequence.splice(
+          incorrectLetterPosition,
+          0,
+          randomLetter,
+          characterSequence[incorrectLetterPosition + 1],
+          characterSequence[incorrectLetterPosition + 2],
+          CHARACTERS.BACKSPACE,
+          CHARACTERS.BACKSPACE,
+          CHARACTERS.BACKSPACE
+        );
+      }
+    }
+
+    const timer = setInterval(() => {
+      switch (characterSequence[currentCharacter.current]) {
+        case CHARACTERS.BACKSPACE:
+          setText((prev) => prev.slice(0, -1));
+          break;
+        case CHARACTERS.WAIT:
+          return;
+        default:
+          setText((prev) => prev + characterSequence[currentCharacter.current]);
+          break;
+      }
+
+      currentCharacter.current += 1;
+      if (currentCharacter.current >= characterSequence.length) {
+        clearInterval(timer);
+      }
+    }, typingInterval);
+
+    return () => clearInterval(timer);
+  }, [availableLetters, children, errorChance, typingInterval]);
+
+  return text;
+};
+
+Typing.propTypes = {
+  /**
+   * String of letters (without spaces) that can be used to write incorrect letter.
+   * Set errorChance argument to number greater that 0 to apply this.
+   */
+  availableLetters: PropTypes.string,
+  /**
+   * String that will be typed.
+   */
+  children: PropTypes.string,
+  /**
+   * Chance to type wrong letter. Varies from 0 to 1.
+   * Child text has to be at least 6 chars long for error change to occur.
+   * If wrong letter is typed, typing will continue by two characters.
+   * After that, three characters will be deleted and typing will continue
+   * with correct letters.
+   */
+  errorChance: PropTypes.number,
+  /**
+   * Interval in miliseconds between each letter.
+   */
+  typingInterval: PropTypes.number,
+};
+
+export default Typing;
---
+++ packages/components/src/components/Typing/index.stories.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import Typing from "./index";
+
+export default {
+  title: "Components/Typing",
+  component: Typing,
+};
+
+export const Story1 = () => {
+  return <Typing>Hello world!</Typing>;
+};
+Story1.storyName = "Basic usage";
+
+export const Story2 = () => {
+  return <Typing typingInterval={1000}>Hello world!</Typing>;
+};
+Story2.storyName = "Slow typing interval";
+Story2.parameters = {
+  docs: {
+    description: {
+      story: `You can adjust typing speed with typingInterval (in milliseconds) argument.`,
+    },
+  },
+};
+
+export const Story3 = () => {
+  return (
+    <Typing errorChance={1} typingInterval={1000}>
+      Hello world!
+    </Typing>
+  );
+};
+Story3.storyName = "Make errors";
+Story3.parameters = {
+  docs: {
+    description: {
+      story: `This Typing component will make error while typing. It will correct itself 
+        by deleting wrong characters. Error can be made only once.`,
+    },
+  },
+};
+
+export const Story4 = () => {
+  return (
+    <>
+      <Typing errorChance={1} typingInterval={100}>
+        This component can type slowly!
+      </Typing>
+      <div />
+      <Typing errorChance={1} typingInterval={100}>
+        This component can type slowly!
+      </Typing>
+      <div />
+      <Typing errorChance={1} typingInterval={100}>
+        This component can type slowly!
+      </Typing>
+      <div />
+      <Typing errorChance={1} typingInterval={100}>
+        This component can type slowly!
+      </Typing>
+      <div />
+      <Typing errorChance={1} typingInterval={100}>
+        This component can type slowly!
+      </Typing>
+      <div />
+      <Typing errorChance={1} typingInterval={100}>
+        This component can type slowly!
+      </Typing>
+      <div />
+      <Typing errorChance={1} typingInterval={100}>
+        This component can type slowly!
+      </Typing>
+      <div />
+      <Typing errorChance={1} typingInterval={100}>
+        This component can type slowly!
+      </Typing>
+      <div />
+      <Typing errorChance={1} typingInterval={100}>
+        This component can type slowly!
+      </Typing>
+      <div />
+      <Typing errorChance={1} typingInterval={100}>
+        This component can type slowly!
+      </Typing>
+      <div />
+    </>
+  );
+};
+Story4.storyName = "Make errors";
+Story4.parameters = {
+  docs: {
+    description: {
+      story: `This story shows multiple typing components, one below another.`,
+    },
+  },
+};
