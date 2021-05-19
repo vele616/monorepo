b11a2058f014d3f0a551a5259a4cffa26e31b2d6
David Abram <david@abram.com>
2021-05-19 10:13:45 +0200

    added awt pages

---
+++ packages/awt/website/pages/index.js
@@ -0,0 +1,40 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/index.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>After Work Talks</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <a
+        href="https://crocoder.dev/"
+        className={styles.supported}
+        rel="nofollow noopener noreferrer"
+        target="_blank"
+      >
+        <Image
+          class={styles.logo}
+          width={80}
+          height={80}
+          alt="CroCoder Logo"
+          src="/crocoder.png"
+        />
+      </a>
+      <main className={styles.layout}>
+        <h1 className={styles.title}>After Work Talks</h1>
+        <div className={styles.topRow}>
+          <div
+            style={{ backgroundColor: "red", gridColumn: "1 / span 1" }}
+          ></div>
+          <div
+            style={{ backgroundColor: "blue", gridColumn: "2 / span 1" }}
+          ></div>
+        </div>
+      </main>
+      <footer></footer>
+    </div>
+  );
+}
