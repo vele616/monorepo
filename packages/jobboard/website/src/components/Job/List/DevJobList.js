1a08231cede02d8cb5f734234e085e579dcecb04
David Abram <david@abram.com>
2021-08-15 20:55:11 +0200

    chore: removed a past quick fix

--- packages/jobboard/website/src/components/Job/List/DevJobList.js
+++ packages/jobboard/website/src/components/Job/List/DevJobList.js
@@ -37,22 +37,18 @@ export const DevJobList = ({ scrollToJobWithIndex }) => (
       }
     `}
     render={(data) => {
-      const bod = new Date();
-      bod.setHours(0, 0, 0, 0);
       return (
         <JobList
           scrollToJobWithIndex={scrollToJobWithIndex}
-          jobs={data.allMarkdownRemark.nodes
-            .filter((node) => node.frontmatter.timestamp <= bod.getTime())
-            .map((node) => ({
-              title: node.frontmatter.title,
-              location: node.frontmatter.location,
-              jobUrl: node.fields.slug,
-              tags: node.frontmatter.hashtags,
-              companyLogo: node.frontmatter.logoUrl,
-              companyName: node.frontmatter.companyName,
-              summary: node.frontmatter.summary,
-            }))}
+          jobs={data.allMarkdownRemark.nodes.map((node) => ({
+            title: node.frontmatter.title,
+            location: node.frontmatter.location,
+            jobUrl: node.fields.slug,
+            tags: node.frontmatter.hashtags,
+            companyLogo: node.frontmatter.logoUrl,
+            companyName: node.frontmatter.companyName,
+            summary: node.frontmatter.summary,
+          }))}
         />
       );
     }}
